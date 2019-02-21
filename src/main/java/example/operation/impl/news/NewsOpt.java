afcf56cf854235fdd6e3d437522f9e6fc3b33e7d
capoon <13929580503@163.com>
2019-02-21 22:30:54 +0800

    66

--- src/main/java/example/operation/impl/news/NewsOpt.java
+++ src/main/java/example/operation/impl/news/NewsOpt.java
@@ -947,9 +947,9 @@ public class NewsOpt {
             String resourceFilePath = GlobalConfig.getProperties(Common.DYNAMICINFOS_SYS_PATH_RESOURCE);
             //检查上传的文件类型，如果是gif，则保留该类型图片
             String fileName = "";
-            String fileOrgName = (String)fileUpload.getName();
+            String fileOrgName = fileUpload.getName();
             NewsOpt.logger.debug(fileOrgName);
-            String fileType =fileOrgName.substring(fileOrgName.lastIndexOf("."),fileOrgName.length());
+            String fileType =fileOrgName.substring(fileOrgName.lastIndexOf(".")+1);
             message = "the file type is: ";
             NewsOpt.logger.debug(message, fileType);
 
